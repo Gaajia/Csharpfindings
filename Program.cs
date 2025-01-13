2098bdceb42a28a42faba8e5b29dc52d0708f598
Gaajia <m.gajia1998@gmail.com>
2025-01-13 09:47:51 +0000

    workshop

--- Program.cs
+++ Program.cs
@@ -1,45 +1,130 @@
-﻿// string[] names = { "Rowena", "Robin", "Bao" };
-// foreach (string name in names)
-// {
-//     Console.WriteLine(name);
-// }
+﻿int examAssignments = 5;
 
-// int[] inventory = { 200, 450, 700, 175, 250 };
-// int sum = 0;
-// int bin = 0;
-// foreach (int items in inventory)
-// {
-//     sum += items;
-//     bin++;
-//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-// }
-// Console.WriteLine($"We have {sum} items in inventory.");
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
-// string[] OrderIds = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
-// foreach (string item in OrderIds)
-// {
-//     if (item.Contains("B"))
-//     {
-//         Console.WriteLine($"Found order with 'B': {item}");
-//     }
-// }
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
-/* Creates OrderIDs for processing
-   Format: Letter(A-E) + 3 digits (e.g., "A123") */
-Random random = new Random();
-string[] orderIDs = new string[5];
+int[] studentScores = new int[10];
 
-for (int i = 0; i < orderIDs.Length; i++)
+string currentStudentLetterGrade = "";
+
+// display the header row for scores/grades
+Console.Clear();
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
+
+/*
+The outer foreach loop is used to:
+- iterate through student names 
+- assign a student's grades to the studentScores array
+- calculate exam and extra credit sums (inner foreach loop)
+- calculate numeric and letter grade
+- write the score report information
+*/
+foreach (string name in studentNames)
 {
-    int prefixValue = random.Next(65, 70);
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    string suffix = random.Next(1, 1000).ToString("000");
+    string currentStudent = name;
 
-    orderIDs[i] = prefix + suffix;
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int gradedAssignments = 0;
+    int gradedExtraCreditAssignments = 0;
+
+    int sumExamScores = 0;
+    int sumExtraCreditScores = 0;
+
+    decimal currentStudentGrade = 0;
+    decimal currentStudentExamScore = 0;
+    decimal currentStudentExtraCreditScore = 0;
+
+    /* 
+    the inner foreach loop: 
+    - sums the exam and extra credit scores
+    - counts the extra credit assignments
+    */
+    foreach (int score in studentScores)
+    {
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+        {
+            sumExamScores = sumExamScores + score;
+        }
+
+        else
+        {
+            gradedExtraCreditAssignments += 1;
+            sumExtraCreditScores += score;
+        }
+    }
+
+    currentStudentExamScore = (decimal)(sumExamScores) / examAssignments;
+    currentStudentExtraCreditScore = (decimal)(sumExtraCreditScores) / gradedExtraCreditAssignments;
+
+    currentStudentGrade = (decimal)((decimal)sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+
+    // Student         Exam Score      Overall Grade   Extra Credit
+    // Sophia          92.2            95.88   A       92 (3.68 pts)
+
+
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCreditScore} ({((decimal)sumExtraCreditScores / 10) / examAssignments} pts)");//+
+
+
 }
 
-foreach (var orderID in orderIDs)
-{
-    Console.WriteLine(orderID);
-}
-
+// required for running in VS Code (keeps the Output windows open to view results)
+Console.WriteLine("\n\rPress the Enter key to continue");
+Console.ReadLine();
--- obj/Debug/net8.0/CsharpLearning1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CsharpLearning1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpLearning1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33aba805f39ce05cf612d73cfb5a9ea49c76a2c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d794ece6468b6a8fb2abc15ce0d64050b3db0de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpLearning1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpLearning1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
