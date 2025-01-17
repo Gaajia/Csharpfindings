597010975d6018aee9756185b20f5af7b6ff5e56
Gaajia <m.gajia1998@gmail.com>
2025-01-17 12:58:37 +0000

    refrence types value types.

--- Program.cs
+++ Program.cs
@@ -1,156 +1,29 @@
-﻿// // string? readResult;
-// // bool validEntry = false;
-// // Console.WriteLine("Enter a string containing at least three characters:");
-// // do
-// // {
-// //     readResult = Console.ReadLine();
-// //     if (readResult != null)
-// //     {
+﻿/*
+In this exercise, you work with integral types. An integral type is a simple value type that represents whole numbers with no fraction (such as -1, 0, 1, 2, 3). The most popular in this category is the int data type.
 
-// //         if (readResult.Length >= 5)
-// //         {
-// //             validEntry = true;
-// //             Console.WriteLine($"Your input '{readResult}' is valid.");
-// //         }
-// //         else
-// //             Console.WriteLine("Your input is invalid, please try again");
-// //     }
+There are two subcategories of integral types: signed and unsigned integral types
+*/
 
-// // } while (validEntry == false);
+// Console.WriteLine("Signed integral types:");
+// Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int   : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long  : {long.MinValue} to {long.MaxValue}");
 
-// // int numericValue = 0;
-// // bool validNumber = false;
-// // validNumber = int.TryParse(readResult, out numericValue);
-// // Console.WriteLine($"Your input {numericValue} {validNumber}");
+// Console.WriteLine("");
+// Console.WriteLine("Unsigned integral types:");
 
-// //CHALLENGES
+// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+// Console.WriteLine("");
+// Console.WriteLine("Floating point types:");
+// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+// Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
 
-// // string? readNumber;
-// // bool validNumber = false;
-// // int numericValue = 0;
-
-// // do
-// // {
-// //     readNumber = Console.ReadLine();
-// //     validNumber = int.TryParse(readNumber, out numericValue);
-
-// //     if (!validNumber)
-// //     {
-// //         Console.WriteLine("Please enter a number");
-// //         break;
-// //     }
-// //     else if (numericValue > 5 && numericValue < 10)
-// //     {
-// //         validNumber = true;
-// //         Console.WriteLine($"Your input ({numericValue}) is valid");
-// //     }
-// //     else {
-// //         Console.WriteLine($"Entered {numericValue} Please enter a numeric value between 5-10");
-// //         break;
-// //         }
+// int[] data;
+// data = new int[3];
 
 
-// // } while (validNumber == false);
-
-// //CHALENGE 2
-
-
-// // string? readLine;
-// // bool validInput = false;
-
-
-// // while (validInput == false)
-// // {
-// //     Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
-// //     readLine = Console.ReadLine();
-// //     readLine = readLine.Trim().ToLower();
-
-// //     switch (readLine)
-// //     {
-// //         case "administrator":
-// //             Console.WriteLine($"Your input value {readLine} has been accepted");
-// //             validInput = true;
-// //             break;
-// //         case "manager":
-// //             Console.WriteLine($"Your input value {readLine} has been accepted");
-// //             validInput = true;
-// //             break;
-// //         case "user":
-// //             Console.WriteLine($"Your input value {readLine} has been accepted");
-// //             validInput = true;
-// //             break;
-// //         default:
-// //             Console.WriteLine($"The role name that you entered,{readLine} is not valid. Enter your role name (Administrator, Manager, or User");
-// //             break;
-
-// //     }
-
-
-// // }
-
-// //Challenge 3 
-
-// /* I like pizza
-// I like roast chicken
-// I like salad
-// I like all three of the menu choices*/
-
-// /* 
-// first go over each element within the array
-//  - find the index of the period in each string
-//  - use the substring method to extract the substring up to the period
-//  - concatenate the extracted substring to a new string
-//  - print the new string
-//  - repeat the process for all elements in the array
-//  - the final string will be the concatenation of all the extracted substrings, each with a period at the end.
-
-// Here is the code:
-
-// */
-
-// string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
-// int periodLocation = 0;
-
-// for (int i = 0; i < myStrings.Length; i++)
-// {
-//     string myString = myStrings[i];
-//     periodLocation = myString.IndexOf(".");
-//     string mySentence;
-
-
-//     while (periodLocation != -1)
-//     {
-//         mySentence = myString.Substring(0, periodLocation);
-//         Console.WriteLine(mySentence);
-//         myString = myString.Remove(0, periodLocation + 1).TrimStart();
-//         periodLocation = myString.IndexOf(".");
-
-
-//     }
-//     if (myString.Length > 0)
-//     {
-//         Console.WriteLine(myString);
-//     }
-
-// }
-
-string[][] jaggedArray = new string[][]
-{
-    new string[] { "one1", "two1", "three1", "four1", "five1", "six1" },
-    new string[] { "one2", "two2", "three2", "four2", "five2", "six2" },
-    new string[] { "one3", "two3", "three3", "four3", "five3", "six3" },
-    new string[] { "one4", "two4", "three4", "four4", "five4", "six4" },
-    new string[] { "one5", "two5", "three5", "four5", "five5", "six5" },
-    new string[] { "one6", "two6", "three6", "four6", "five6", "six6" },
-    new string[] { "one7", "two7", "three7", "four7", "five7", "six7" },
-    new string[] { "one8", "two8", "three8", "four8", "five8", "six8" }
-};
-
-foreach (string[] array in jaggedArray)
-{
-    foreach (string value in array)
-    {
-        Console.WriteLine(value);
-    }
-    Console.WriteLine();
-}
