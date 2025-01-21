6d7477cf364af3e12bb7a96c23cf0fe0e6b94cd6
Gaajia <m.gajia1998@gmail.com>
2025-01-21 10:38:44 +0000

    converting data types using casting done

--- Program.cs
+++ Program.cs
@@ -1,29 +1,90 @@
-﻿/*
-In this exercise, you work with integral types. An integral type is a simple value type that represents whole numbers with no fraction (such as -1, 0, 1, 2, 3). The most popular in this category is the int data type.
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
 
-There are two subcategories of integral types: signed and unsigned integral types
-*/
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-// Console.WriteLine("Signed integral types:");
-// Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
-// Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
-// Console.WriteLine($"int   : {int.MinValue} to {int.MaxValue}");
-// Console.WriteLine($"long  : {long.MinValue} to {long.MaxValue}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-// Console.WriteLine("");
-// Console.WriteLine("Unsigned integral types:");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
 
-// Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-// Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-// Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-// Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
-// Console.WriteLine("");
-// Console.WriteLine("Floating point types:");
-// Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-// Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-// Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
 
-// int[] data;
-// data = new int[3];
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
 
 
+
+// string value = "30";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//     Console.WriteLine($"Measurement: {result}");
+
+// }
+// else
+// {
+//     Console.WriteLine("Unable to report the measurement.");
+// }
+// if (result > 0)
+// {
+//     Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+// }
+
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// double result = 0;
+// string charac = "";
+
+// for (int i = 0; i < values.Length; i++)
+// {
+
+//     if (double.TryParse(values[i], out double number))
+//     {
+//         result += number;
+//     }
+//     else
+//     {
+//         charac += values[i];
+//     }
+
+
+// }
+
+// Console.WriteLine($"{result} , {charac}");
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int:");
+if (value1 % 2 != 0)
+{
+    int result = Convert.ToInt32(value1 / value2);
+
+    Console.WriteLine(result);
+}
+
+// Your code here to set result2
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal:");
+if (value3 != 0)
+{
+
+    decimal result2 = value2 / Convert.ToDecimal(value3);
+    Console.WriteLine($"Result as decimal: {result2}");
+}
+
+// Your code here to set result3
+Console.WriteLine($"Divide value3 by value1, display the result as a float: ");
+if (value3 != 0)
+{
+    float result3 = value3 / value1;
+    Console.WriteLine($"{result3}");
+}
